029f0b36c3e0d3b10718dc72c74d42acee1d3f80
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-10 11:26:08 +0500

    commit

--- login/index.php
+++ login/index.php
@@ -86,8 +86,10 @@ if (!empty($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../src/css/login_signup.css">
+    <link rel="icon" type="image/x-icon" href="../favicon.ico">
     <title>Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="../src/css/login_signup.css">
 </head>
 
 <body>
@@ -97,18 +99,59 @@ if (!empty($_POST['submit'])) {
             <div class="form-group">
                 <label for="username">Username</label>
                 <input type="text" id="username" name="username" required maxlength="30">
+                <small id="username-error" style="color: red;"></small>
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
-                <input type="password" id="password" name="password" required maxlength="255">
+                <div class="password-container">
+                    <input type="password" id="password" name="password" required maxlength="255">
+                    <button type="button" id="toggle-password" class="password-toggle"><i
+                            class="fas fa-eye"></i></button>
+                </div>
             </div>
             <div class="form-group">
-                <button type="submit" name="submit">Login</button>
+                <button type="submit" name="submit" id="submit" disabled>Login</button>
             </div>
         </form>
-        <p>Don't have an account? <a href="../signup/">Sign Up</a></p>
+        <div class="text-center">
+            <p>Don't have an account? <a href="../signup/">Sign Up</a></p>
+        </div>
     </div>
     <script src="../src/js/sweetalert2.js"></script>
+    <script>
+        const usernameField = document.getElementById('username');
+        const usernameError = document.getElementById('username-error');
+        const submitButton = document.getElementById('submit');
+
+        function validateForm() {
+            const username = usernameField.value;
+            const usernamePattern = /^[a-zA-Z0-9_]+$/;
+            if (!usernamePattern.test(username)) {
+                usernameError.textContent = "Username can only contain letters, numbers, and underscores!";
+                submitButton.disabled = true;
+            } else {
+                usernameError.textContent = "";
+                submitButton.disabled = false;
+            }
+        }
+
+        usernameField.addEventListener('input', validateForm);
+
+        document.getElementById('toggle-password').addEventListener('click', function () {
+            const passwordField = document.getElementById('password');
+            const toggleIcon = this.querySelector('i');
+
+            if (passwordField.type === 'password') {
+                passwordField.type = 'text';
+                toggleIcon.classList.remove('fa-eye');
+                toggleIcon.classList.add('fa-eye-slash');
+            } else {
+                passwordField.type = 'password';
+                toggleIcon.classList.remove('fa-eye-slash');
+                toggleIcon.classList.add('fa-eye');
+            }
+        });
+    </script>
 </body>
 
 </html>
