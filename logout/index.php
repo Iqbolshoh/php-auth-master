afd9ea6d5e70ac7b0d57d20f4c8123e9561ec25f
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-10 10:44:59 +0500

    commit

--- logout/index.php
+++ logout/index.php
@@ -3,6 +3,9 @@ session_start();
 session_unset();
 session_destroy();
 
+$session_token = session_id();
+$query->delete('active_sessions', 'session_token = ?', [$session_token], 's');
+
 $cookies = ['username', 'session_token'];
 foreach ($cookies as $cookie) {
     if (isset($_COOKIE[$cookie])) {
