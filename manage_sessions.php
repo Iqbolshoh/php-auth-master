c6068cd6bdaf8f33fb7f6591c96fe4d51b2ea204
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-11 13:54:03 +0500

    commit

--- manage_sessions.php
+++ manage_sessions.php
@@ -20,30 +20,7 @@ $query = new Database();
 </head>
 
 <body>
-    <h1>Active Sessions</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>Device Name</th>
-                <th>IP Address</th>
-                <th>Last Activity</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $sessions = $query->select('active_sessions', '*', 'user_id = ?', [$_SESSION['user_id']], 'i');
-            foreach ($sessions as $session) {
-                echo "<tr>";
-                echo "<td>{$session['device_name']}</td>";
-                echo "<td>{$session['ip_address']}</td>";
-                echo "<td>{$session['last_activity']}</td>";
-                echo "<td><a href='remove_session.php?token={$session['session_token']}'>Remove</a></td>";
-                echo "</tr>";
-            }
-            ?>
-        </tbody>
-    </table>
+    
 </body>
 
 </html>
