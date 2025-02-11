fa1d145203260279efe9651d2d7707c4e142c2ba
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-11 13:03:42 +0500

    commit

--- admin/index.php
+++ admin/index.php
@@ -17,7 +17,7 @@ $query = new Database();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <link rel="icon" type="image/x-icon" href="../favicon.ico">
     <link rel="stylesheet" href="../src/css/adminlte.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
@@ -106,11 +106,11 @@ $query = new Database();
     </div>
 
     <!-- jQuery -->
-    <script src="./src/js/jquery.min.js"></script>
+    <script src="../src/js/jquery.min.js"></script>
 
     <!-- Bootstrap 4 -->
-    <script src="./src/js/bootstrap.bundle.min.js"></script>
-    <script src="./src/js/adminlte.min.js"></script>
+    <script src="../src/js/bootstrap.bundle.min.js"></script>
+    <script src="../src/js/adminlte.min.js"></script>
 </body>
 
 </html>
