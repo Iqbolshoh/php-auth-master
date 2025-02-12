401f6e435d4e12a53053b08bc9f9f0740deee5b6
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-12 10:33:18 +0500

    commit

--- login/index.php
+++ login/index.php
@@ -159,12 +159,10 @@ if (
 
             if (passwordField.type === 'password') {
                 passwordField.type = 'text';
-                toggleIcon.classList.remove('fa-eye');
-                toggleIcon.classList.add('fa-eye-slash');
+                toggleIcon.classList.replace('fa-eye', 'fa-eye-slash');
             } else {
                 passwordField.type = 'password';
-                toggleIcon.classList.remove('fa-eye-slash');
-                toggleIcon.classList.add('fa-eye');
+                toggleIcon.classList.replace('fa-eye-slash', 'fa-eye');
             }
         });
     </script>
