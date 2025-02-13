35ee911e5c13097d5a0430b62ad10cc47b0bfef5
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-13 11:20:23 +0500

    commit

--- signup/index.php
+++ signup/index.php
@@ -107,6 +107,7 @@ if (
             'user_id' => $user_id,
             'device_name' => get_user_info(),
             'ip_address' => $_SERVER['REMOTE_ADDR'],
+            'last_activity' => date('Y-m-d H:i:s'),
             'session_token' => session_id()
         ]);
 
