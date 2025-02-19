73b6f25c9b52e34ddb1070bd009a805389d91b5e
iqbolshoh <iilhomjonov777@gmail.com>
2025-02-19 21:56:19 +0500

    commit

--- active_sessions.php
+++ active_sessions.php
@@ -33,9 +33,8 @@ if (isset($_POST['update_session'])) {
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./src/css/adminlte.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
